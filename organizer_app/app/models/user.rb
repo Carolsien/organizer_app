40e50cc246ede3c80e0c1733fc91689704e2c9b1
Karolina <karolina.rostek.93@gmail.com>
2016-05-24 01:00:44 +0200

    dodanie rejestracji

--- organizer_app/app/models/user.rb
+++ organizer_app/app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token
+  attr_accessor :remember_token,
+                :activation_token,
+                :reset_token
+
+  before_save   :downcase_email
+  before_create :create_activation_digest
+
   before_save { self.email = email.downcase }
   validates :name,  presence: true,
                     length: { maximum: 50 },
@@ -16,9 +22,14 @@ class User < ActiveRecord::Base
             length: { minimum: 6 },
             length: {maximum: 25 }
 
+   validates :password, presence: true,
+                        length: { minimum: 6 },
+                        allow_nil: true
+
+
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
+                                                                        BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
@@ -32,39 +43,55 @@ class User < ActiveRecord::Base
  end
 
  # Returns true if the given token matches the digest.
-   def authenticated?(remember_token)
-     return false if remember_digest.nil?
-     BCrypt::Password.new(remember_digest).is_password?(remember_token)
-   end
+  def authenticated?(attribute, token)
+   digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
 
    # Forgets a user.
    def forget
      update_attribute(:remember_digest, nil)
    end
 
-   # Returns the hash digest of the given string.
-   def self.digest(string)
-     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                BCrypt::Engine.cost
-     BCrypt::Password.create(string, cost: cost)
-   end
 
-# Returns a random token.
-  def self.new_token
-    SecureRandom.urlsafe_base64
+ # Activates an account.
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
   end
 
-  class << self
-   # Returns the hash digest of the given string.
-   def digest(string)
-     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                   BCrypt::Engine.cost
-     BCrypt::Password.create(string, cost: cost)
-   end
+  # Sends activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
 
-   # Returns a random token.
-   def new_token
-     SecureRandom.urlsafe_base64
-   end
- end
+  # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
+ private
+
+# Converts email to all lower-case.
+  def downcase_email
+    self.email = email.downcase
+  end
+
+    # Creates and assigns the activation token and digest.
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
 end
