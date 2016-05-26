6fdfb29332f89ab13a108e2035fa9a1dba9e864b
Karolina <karolina.rostek.93@gmail.com>
2016-05-26 10:17:30 +0200

    dodanie static pages

--- organizer_app/db/schema.rb
+++ organizer_app/db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160523192532) do
+ActiveRecord::Schema.define(version: 20160524004513) do
+
+  create_table "guests", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
