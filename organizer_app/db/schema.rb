ad88f174256499ae898265cc90776197ef355c67
ABruchwalska <abruchwalska@wp.pl>
2016-06-02 12:24:42 +0200

    Poprawienie rejestracji

--- organizer_app/db/schema.rb
+++ organizer_app/db/schema.rb
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20160524004513) do
     t.string   "remember_digest"
     t.boolean  "admin",             default: false
     t.string   "activation_digest"
+    t.boolean  "activated",         default: false
     t.datetime "activated_at"
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
